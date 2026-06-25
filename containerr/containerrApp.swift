b8bf4c7dc1878675fa5e7655b3e20eb060f44842
Naveed <46922100+nedanwr@users.noreply.github.com>
2026-06-25 16:17:58 +0800

    chore: initial commit

---
+++ containerr/containerrApp.swift
@@ -0,0 +1,17 @@
+//
+//  containerrApp.swift
+//  containerr
+//
+//  Created by Naveed Ali Anwar on 6/25/26.
+//
+
+import SwiftUI
+
+@main
+struct containerrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ containerrTests/containerrTests.swift
@@ -0,0 +1,17 @@
+//
+//  containerrTests.swift
+//  containerrTests
+//
+//  Created by Naveed Ali Anwar on 6/25/26.
+//
+
+import Testing
+@testable import containerr
+
+struct containerrTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
